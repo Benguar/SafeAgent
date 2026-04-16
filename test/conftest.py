3a2b1381b5e308a59280c40463d54c73a73af75d
Benguar <iqmbenzy@gmail.com>
2026-04-16 08:34:38 +0100

    test changes

--- test/conftest.py
+++ test/conftest.py
@@ -3,8 +3,15 @@ import pytest_asyncio
 from fastapi.testclient import TestClient
 from unittest.mock import patch
 from src.main import app, lifespan
+from src.db.connection import engine
+from src.db.models import Base
 from httpx import AsyncClient, ASGITransport
 
+@pytest_asyncio.fixture(scope="session", autouse=True)
+async def setup_blank_test_database():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
 
 @pytest_asyncio.fixture(scope="session")
 async def client():
